54e09c203a5a2c623e1686ebb080735a35e53b64
Maria Molchanova <molchmd@gmail.com>
2024-05-21 17:14:33 +0300

    Задание 3. Удалено создание меню

--- src/main/java/ru/molchmd/telegrambot/MiniBankTelegramBot.java
+++ src/main/java/ru/molchmd/telegrambot/MiniBankTelegramBot.java
@@ -4,18 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.commands.DeleteMyCommands;
-import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
-import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.molchmd.telegrambot.commands.Command;
-import ru.molchmd.telegrambot.commands.ICommand;
 import ru.molchmd.telegrambot.handler.MessageHandler;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 @Slf4j
@@ -31,8 +22,6 @@ public class MiniBankTelegramBot extends TelegramLongPollingBot {
         this.name = name;
         this.token = token;
         this.messageHandler = messageHandler;
-
-        createMenu(true);
     }
 
     @Override
@@ -53,32 +42,5 @@ public class MiniBankTelegramBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
         return name;
-    }
-
-    private void createMenu(boolean isCreate) {
-        if (!isCreate) {
-            try {
-                execute(new DeleteMyCommands(new BotCommandScopeDefault(), "ru"));
-                log.info("Menu was deleted successfully");
-            }
-            catch (TelegramApiException e) {
-                log.error("Menu was not deleted");
-            }
-            return;
-        }
-
-        List<BotCommand> menu = new ArrayList<>();
-        for (Command command : Command.values()) {
-            if (command.isDisplayToMenu)
-                menu.add(new BotCommand(command.name, command.description));
-        }
-
-        try {
-            execute(new SetMyCommands(menu, new BotCommandScopeDefault(), "ru"));
-            log.info("Menu was created successfully, command count - {}", menu.size());
-        }
-        catch (TelegramApiException e) {
-            log.error("Menu was not created");
-        }
     }
 }
