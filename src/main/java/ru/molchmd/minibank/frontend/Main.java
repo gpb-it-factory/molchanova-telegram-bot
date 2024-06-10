30c81d5a6dd68ef3915ddbfa67fd2b4608a7aadd
Maria Molchanova <molchmd@gmail.com>
2024-06-10 23:40:46 +0300

    Задание 6. Файлы перенесены в дополнительную общую папку

--- src/main/java/ru/molchmd/minibank/frontend/Main.java
+++ src/main/java/ru/molchmd/minibank/frontend/Main.java
@@ -1,4 +1,4 @@
-package ru.molchmd;
+package ru.molchmd.minibank.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/ru/molchmd/minibank/frontend/telegrambot/commands/Command.java
+++ src/main/java/ru/molchmd/minibank/frontend/telegrambot/commands/Command.java
@@ -1,9 +1,10 @@
-package ru.molchmd.telegrambot.commands;
+package ru.molchmd.minibank.frontend.telegrambot.commands;
 
 public enum Command {
     START("/start", "стартовое сообщение", false),
     HELP("/help", "список доступных команд", true),
-    PING("/ping", "отвечу pong", true);
+    PING("/ping", "отвечу pong", false),
+    REGISTER("/register", "зарегистрироваться", true);
 
     public final String name;
     public final String description;
